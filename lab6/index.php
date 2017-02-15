d54c11046e8150fb8d10d2d29de6c854858568c3
Nastya <nastya.gorobets95@gmail.com>
2017-02-15 13:12:55 +0200

    Changed regular

--- lab6/index.php
+++ lab6/index.php
@@ -15,13 +15,15 @@ $str1="test@otp.khpi.ua";
 $str2="test2@otp.khpi.ua";
 $str3="@otp.kphi.ua";
 $str4="email4_@otp.khpi.ua";
-$reg="/^[A-z0-9_]+(@otp.khpi.ua)$/";
+$str5="email_4.htrjhrk@otp.khpi.ua";
+$reg="/^[A-z0-9]+\.?[A-z0-9]+(@otp.khpi.ua)$/";
 $mat=[];
 $count1=preg_match_all($reg,$str1);
 $count2=preg_match_all($reg,$str2);
 $count3=preg_match_all($reg,$str3);
 $count4=preg_match_all($reg,$str4);
-echo "Res1 for <span>$str1</span>=$count1<br> Res2 for <span>$str2</span>=$count2<br> Res3 for <span>$str3</span>=$count3<br> Res4 for <span>$str4</span>=$count4";
+$count5=preg_match_all($reg,$str5);
+echo "Res1 for <span>$str1</span>=$count1<br> Res2 for <span>$str2</span>=$count2<br> Res3 for <span>$str3</span>=$count3<br> Res4 for <span>$str4</span>=$count4 <br> Res5 for <span>$str5</span>=$count5";
 ?>
 </body>
 </html>
