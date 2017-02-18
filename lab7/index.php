9542da924d1e3f126d35eb5621a946df79ee9a18
Nastya <nastya.gorobets95@gmail.com>
2017-02-18 22:29:27 +0200

    Changed Index page

--- lab7/index.php
+++ lab7/index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <title>Lab7</title>
     <link rel="stylesheet" href="css/main.css">
+    <script src="../js/jquery-3.1.1.js"></script>
+
+    <script src="../js/jBox-0.4.7/jBox-0.4.7/Source/jBox.min.js"></script>
+    <link href="../js/jBox-0.4.7/jBox-0.4.7/Source/jBox.css" rel="stylesheet">
 </head>
 <body>
 <?php
@@ -11,51 +15,66 @@ require_once '../lab4/LabInfo.php';
 $labInfo=new LabInfo();
 $labInfo->printInfo("7","Робота з формами","02-17-2017","Розробити форму, яка обробляє введені користувачем дані. Пароль: від 7-ми до 10ти символів, має містити заголовну літеру. Логін не можна вводити двічі.");
 if(isset($_REQUEST['submit'])){
-    if($_REQUEST['password'] ==  $_REQUEST['password2']){
-     $password=$_REQUEST['password'];
-        $login=$_REQUEST['login'];
-        if(empty($_COOKIE['login'])){
-            setcookie('login',$login);
-        }
-         else {
-             if ($login == $_COOKIE['login']) {
-                 echo "<div class='error'>Login is busy</div><br>";
-             }
-            else{
-                setcookie('login',$login);
-                $arr=str_split($password);
-                $numberCount=0; //count of numbers
-                $upperFlag=false; //flag for Upper symbol
-                $countFlag=false; //count
-                foreach ($arr as $a){
-                    if(is_numeric($a))$numberCount++;
-                    else {
-                        if(strtoupper($a) == $a) {
-                            $upperFlag = true;
-                        }
-                    }
-                }
-                $symbolCount=count($arr);
-                if($symbolCount>7 && $symbolCount<10)$countFlag=true;
-                if($numberCount>=2 && $upperFlag && $countFlag) {
-                    echo "<div class='correct'>All is correct!</div><br>";
-                }
-            }
-            }
-        } else{
+    $password1=$_REQUEST['password'];
+    $password2=$_REQUEST['password'];
+    $login=$_REQUEST['login'];
+    if($password1 ==  $password2){
+               if($login == $password1){
+                   echo  "Login one time!<br>";
+               }else {
+                   $arr = str_split($password1);
+                   $numberCount = 0; //count of numbers
+                   $upperFlag = false; //flag for Upper symbol
+                   $countFlag = false; //count
+                   foreach ($arr as $a) {
+                       if (is_numeric($a)) $numberCount++;
+                       else {
+                           if (strtoupper($a) == $a) {
+                               $upperFlag = true;
+                           }
+                       }
+                   }
+                   $symbolCount = count($arr);
+                   if ($symbolCount > 7 && $symbolCount <= 10) $countFlag = true;
+                   if ($numberCount >= 2 && $upperFlag && $countFlag) {
+                       echo "<div class='correct'>All is correct!</div><br>";
+                   }
+               }
+            }else{
         echo "<div class='error'>The password must be the same</div><br>";
     }
-}
+
+            }
+
 ?>
+
+
+
+
 <form action="" method="get">
-    <label for="login">Login:</label>
+    <label for="login"><span class="star">* </span>Login:</label>
     <input type="text" name="login" id="name"><br>
-    <label for="password">Password:</label>
-    <input type="password" name="password"><br>
-    <label for="password2">Password2:</label>
-    <input type="password" name="password2"><br><br>
+    <label for="password"><span class="star">* </span>Password:</label>
+    <input type="password" name="password"> <span class="tooltip" title="Password from 7 to 10 characters. Must be an uppercase character. And a couple of digits">?</span>
+    <br>
+    <label for="password2"><span class="star">* </span>Password2:</label>
+    <input type="password" name="password2"> <span class="tooltip" title="The password must be the same as the password above">?</span><br><br>
     <input type="submit" name="submit" id="submit" value="Send">
 </form>
+<script>
+    $(function() {
+
+        new jBox('Tooltip', {
+            attach: '.tooltip',
+            adjustPosition: true
+        });
+
+        $("#passwordInfo").click(function () {
+
+
+        });
+    });
+</script>
 </body>
 </html>
 
