fde9f9c1f36ae18784a12e3294058e7bfa76301d
Nastya <nastya.gorobets95@gmail.com>
2017-02-19 17:10:20 +0200

    Refactoring

--- lab8/index.php
+++ lab8/index.php
@@ -11,34 +11,32 @@
 <body>
 <?php
 require_once '../lab4/LabInfo.php';
-$labInfo=new LabInfo();
-$labInfo->printInfo(8,"Файли","02-19-2017","Прочитати зміст файлу. Якщо рядок починається з S, то вивести його посимвольно");
-if(isset($_REQUEST['send'])){
-    $text="";
-    $res="";
-    $filename=$_REQUEST['fileName'];
-    if(file_exists($filename)){
-        $file=fopen($filename,"r");
-
-        $buffer=[];
-        while(!feof($file))
-            $buffer[]= fgets($file);
-
-        foreach ($buffer as $buf){
-            $text.=$buf."<br>";
-            if($buf[0] == "S"){
-                $arr=str_split($buf);
-                foreach ($arr as $a){
-                    $res.=$a."  ";
+$labInfo = new LabInfo();
+$labInfo->printInfo(8, "Файли", "02-19-2017", "Прочитати зміст файлу. Якщо рядок починається з S, то вивести його посимвольно");
+if (isset($_REQUEST['send'])) {
+    $text = "";
+    $res = "";
+    $filename = $_REQUEST['fileName'];
+    if (file_exists($filename)) {
+        $file = fopen($filename, "r");
+        $buffer = [];
+        while (!feof($file))
+            $buffer[] = fgets($file);
+        foreach ($buffer as $buf) {
+            $text .= $buf . "<br>";
+            if ($buf[0] == "S") {
+                $arr = str_split($buf);
+                foreach ($arr as $a) {
+                    $res .= $a . "  ";
                 }
-                $res.="<br>";
+                $res .= "<br>";
             }
         }
-        $res="<span>Result is :</span> <br> $res <br>";
-        $text="<span>Text in file :</span> <br> $text  <br>";
+        $res = "<span>Result is :</span> <br> $res <br>";
+        $text = "<span>Text in file :</span> <br> $text  <br>";
 
         fclose($file);
-    }else {
+    } else {
         echo "<span class='error'>$filename file  not found</span><br>";
     }
 }
@@ -47,21 +45,16 @@ if(isset($_REQUEST['send'])){
     <input type="text" name="fileName" id="fileName" placeholder="Enter file name">
     <input type="submit" id="send" name="send" value="Send">
 </form>
-
-<?php if (!empty($text) && !empty($res)){?>
+<?php if (!empty($text) && !empty($res)) { ?>
     <div class="result">
-    <?= $text ?>
-      </div>
+        <?= $text ?>
+    </div>
 
-<div class="result">
-    <?= $res ?>
-</div>
-  <?php
-}?>
-
-
-
-
+    <div class="result">
+        <?= $res ?>
+    </div>
+    <?php
+} ?>
 </body>
 </html>
 
---
+++ lab9/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nastya
+ * Date: 19.02.2017
+ * Time: 16:30
+ */
