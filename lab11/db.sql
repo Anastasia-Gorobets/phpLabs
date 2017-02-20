5862d2d77088ec8270d5553fb833f9b999143289
Nastya <nastya.gorobets95@gmail.com>
2017-02-20 21:56:35 +0200

    Changed script

--- lab11/db.sql
+++ lab11/db.sql
@@ -1,12 +1,22 @@
 CREATE DATABASE operator_services;
 USE operator_services;
 
+CREATE TABLE tariffs (
+  id  INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE=InnoDB CHARACTER SET=UTF8;
+
+
 CREATE TABLE clients (
   id  INT AUTO_INCREMENT NOT NULL,
-  tariff VARCHAR(255) NOT NULL NULL,
-  validity VARCHAR(255) NOT NULL,
+  tariff_id INT NOT NULL,
+  activation_date DATETIME NOT NULL ,
   phoneNumber VARCHAR(255) NOT NULL ,
-  PRIMARY KEY(id)
+  PRIMARY KEY(id),
+  FOREIGN KEY (tariff_id) REFERENCES tariffs(id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
 ) ENGINE=InnoDB CHARACTER SET=UTF8;
 
 CREATE TABLE services (
@@ -41,11 +51,17 @@ CREATE TABLE personal_information(
     ON DELETE RESTRICT
 ) ENGINE=InnoDB CHARACTER SET=UTF8;
 
-INSERT INTO clients (id,tariff,validity,phoneNumber) VALUES (1,"tariff1","10-02-2017","380938474271");
-INSERT INTO clients (id,tariff,validity,phoneNumber) VALUES (2,"tariff1","10-02-2017","380938474272");
-INSERT INTO clients (id,tariff,validity,phoneNumber) VALUES (3,"tariff2","12-01-2017","380938474273");
-INSERT INTO clients (id,tariff,validity,phoneNumber) VALUES (4,"tariff3","10-01-2017","380938474274");
-INSERT INTO clients (id,tariff,validity,phoneNumber) VALUES (5,"tariff3","11-02-2017","380938474275");
+
+INSERT INTO tariffs (id,name) VALUES (1,"tariff1");
+INSERT INTO tariffs (id,name) VALUES (2,"tariff2");
+INSERT INTO tariffs (id,name) VALUES (3,"tariff3");
+INSERT INTO tariffs (id,name) VALUES (4,"tariff4");
+
+INSERT INTO clients (id,tariff_id,activation_date,phoneNumber) VALUES (1,1,"2015-02-12 12:12:12","380938474271");
+INSERT INTO clients (id,tariff_id,activation_date,phoneNumber) VALUES (2,2,"2015-04-10 12:12:12","380938474272");
+INSERT INTO clients (id,tariff_id,activation_date,phoneNumber) VALUES (3,1,"2017-02-12 13:12:12","380938474273");
+INSERT INTO clients  (id,tariff_id,activation_date,phoneNumber) VALUES (4,3,"2016-02-12 14:12:12","380938474274");
+INSERT INTO clients (id,tariff_id,activation_date,phoneNumber) VALUES (5,3,"2014-07-09 18:19:12","380938474275");
 
 INSERT INTO  services(id,name,category) VALUES (1,"service1","category1");
 INSERT INTO  services(id,name,category) VALUES (2,"service2","category1");
@@ -64,7 +80,6 @@ INSERT INTO personal_information(id,client_id,full_name,address,region) VALUES (
 INSERT INTO personal_information(id,client_id,full_name,address,region) VALUES (3,3,"fio3","addr3","region2");
 INSERT INTO personal_information(id,client_id,full_name,address,region) VALUES (4,4,"fio4","addr4","region3");
 INSERT INTO personal_information(id,client_id,full_name,address,region) VALUES (5,5,"fio5","addr5","region2");
-
 
 
 
