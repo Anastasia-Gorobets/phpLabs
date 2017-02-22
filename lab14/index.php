e4d9651f51a30585784f044af85d78f569719c07
Nastya <nastya.gorobets95@gmail.com>
2017-02-22 20:35:23 +0200

    Added lab14

---
+++ lab14/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../lab4/LabInfo.php';
+$labInfo=new LabInfo();
+$labInfo->printInfo(14,"Работа с графикой","22-02-2017","Вивести графік з табличних даних");
+require_once 'image.php';
+?>
+<img src="res.png">
