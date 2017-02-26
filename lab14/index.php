b419e2d4a866cc055f9b9388b39a548c9feb00b7
ReenExe <ipourri@gmail.com>
2017-02-26 23:27:15 +0200

    правильнее в методе возвращать строку, а вывод делать уже в "view" $labInfo = new LabInfo(); echo $labInfo->getInfo("...");

--- lab14/index.php
+++ lab14/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../lab4/LabInfo.php';
-$labInfo=new LabInfo();
-$labInfo->printInfo(14,"Работа с графикой","22-02-2017","Вивести графік з табличних даних");
+$labInfo = new LabInfo();
+echo $labInfo->getInfo(14, "Работа с графикой", "22-02-2017", "Вивести графік з табличних даних");
 require_once 'image.php';
 ?>
 <img src="res.png">
--- lab4/LabInfo.php
+++ lab4/LabInfo.php
@@ -1,11 +1,24 @@
 <?php
+
 class LabInfo
 {
-    public function printInfo($number,$subject,$date,$task){
-        echo "<p>Лабораторна робота  № $number</p>";
-        echo "<p>Тема: $subject</p>";
-        echo "<p>Дата: $date</p>";
-        echo "<p>Завдання: $task</p>";
-        echo "<p>Горобець  Анастасія Олександрівна, КІТ22Б</p>";
+    /**
+     * @param $number
+     * @param $subject
+     * @param $date
+     * @param $task
+     */
+    public function printInfo($number, $subject, $date, $task)
+    {
+        echo $this->getInfo($number, $subject, $date, $task);
+    }
+
+    public function getInfo($number, $subject, $date, $task)
+    {
+        return "<p>Лабораторна робота  № $number</p>"
+            . "<p>Тема: $subject</p>"
+            . "<p>Дата: $date</p>"
+            . "<p>Завдання: $task</p>"
+            . "<p>Горобець  Анастасія Олександрівна, КІТ22Б</p>";
     }
 }
