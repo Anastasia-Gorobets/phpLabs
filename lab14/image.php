79b0f12b4224bc5053af43e72c30868866d8b68e
ReenExe <ipourri@gmail.com>
2017-02-26 23:32:03 +0200

    Вместо дублирование значения `res.png` сделал явно создания картинки и ее вывод в одном файле

--- lab14/image.php
+++ lab14/image.php
@@ -1,20 +1,27 @@
 <?php
 //Include the code
 require_once 'phplot-6.2.0/phplot-6.2.0/phplot.php';
-//Define the object
-$plot = new PHPlot(800,800,'res.png');
-$plot->SetIsInline(True);
-//Define some data
-$example_data=[];
-for($i=0;$i<=35;$i++){
-    $example_data[]=[$i,mt_rand(5,50)];
+
+class Image
+{
+    public function create($name)
+    {
+        //Define the object
+        $plot = new PHPlot(800, 800, $name);
+        $plot->SetIsInline(True);
+        //Define some data
+        $example_data = [];
+        for ($i = 0; $i <= 35; $i++) {
+            $example_data[] = [$i, mt_rand(5, 50)];
+        }
+        $plot->SetDataColors('green');
+        $plot->SetDataValues($example_data);
+        //Turn off X axis ticks and labels because they get in the way:
+        $plot->SetXTickLabelPos('none');
+        $plot->SetXTickPos('none');
+        $plot->SetTitle("Plot example");
+        $plot->SetXTitle('X Data');
+        $plot->SetYTitle('Y Data');
+        $plot->DrawGraph();
+    }
 }
-$plot->SetDataColors('green');
-$plot->SetDataValues($example_data);
-//Turn off X axis ticks and labels because they get in the way:
-$plot->SetXTickLabelPos('none');
-$plot->SetXTickPos('none');
-$plot->SetTitle("Plot example");
-$plot->SetXTitle('X Data');
-$plot->SetYTitle('Y Data');
-$plot->DrawGraph();
--- lab14/index.php
+++ lab14/index.php
@@ -1,7 +1,10 @@
 <?php
-require_once '../lab4/LabInfo.php';
-$labInfo = new LabInfo();
-echo $labInfo->getInfo(14, "Работа с графикой", "22-02-2017", "Вивести графік з табличних даних");
-require_once 'image.php';
+    require_once '../lab4/LabInfo.php';
+    $labInfo = new LabInfo();
+    echo $labInfo->getInfo(14, "Работа с графикой", "22-02-2017", "Вивести графік з табличних даних");
+    require_once 'image.php';
+    $image = new Image();
+    $src = 'res.png';
+    $image->create('res.png');
 ?>
-<img src="res.png">
+<img src="<?php echo $src;?>">
